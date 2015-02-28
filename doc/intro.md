d1c658375a4b11e834f578a66b46c4ef20cc0972
Timothy Renner <renner.timothy@gmail.com>
2015-02-28 08:26:14 -0600

    Initial commit. Blank project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tweetshovel
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tweetshovel/core_test.clj
@@ -0,0 +1,7 @@
+(ns tweetshovel.core-test
+  (:require [clojure.test :refer :all]
+            [tweetshovel.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
